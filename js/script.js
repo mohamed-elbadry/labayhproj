c9e62263eb63a5a8b3308a7367a1d22a760bd0e8
mohamed-elbadry <mohmedelbadry25@gmail.com>
2024-06-24 01:38:03 +0300

    busi

--- js/script.js
+++ js/script.js
@@ -163,3 +163,24 @@ $('#team').owlCarousel({
           }
         });
       })
+
+
+      $('.slider').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin:30,
+        nav:false,
+        dots: false,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+      })
